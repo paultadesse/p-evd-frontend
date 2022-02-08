7cb3c121abf6b8aaaa4f7a99cfad4af4da2a476a
Paulos Tadesse <paulosta952@gmail.com>
2022-02-08 14:19:57 +0300

    :rocket: create dashboard components

--- src/components/layouts/SideBar.vue
+++ src/components/layouts/SideBar.vue
@@ -23,7 +23,7 @@
 				<div class="ml-9 pt-4 space-y-4 text-sm">
 					<div class="text-gray-400">Analytics</div>
 					<div class="text-gray-400">CRM</div>
-					<div class="text-white">Vouchers</div>
+					<div class="text-white">Status</div>
 					<div class="text-gray-400">Sales</div>
 				</div>
 			</div>
